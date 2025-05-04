a71a5e41a00eff3647c7823d2840c6a13ce1e18b
suiwave <184898096+suiwave@users.noreply.github.com>
2025-05-04 14:19:30 +0900

    lambda関数まで疎通

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,44 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/archive" {
+  version = "2.7.0"
+  hashes = [
+    "h1:YkXq4JfcoAW0L4B9ghskZUxYbYAXIPlfSqqVFrAS06U=",
+    "zh:04e23bebca7f665a19a032343aeecd230028a3822e546e6f618f24c47ff87f67",
+    "zh:5bb38114238e25c45bf85f5c9f627a2d0c4b98fe44a0837e37d48574385f8dad",
+    "zh:64584bc1db4c390abd81c76de438d93acf967c8a33e9b923d68da6ed749d55bd",
+    "zh:697695ab9cce351adf91a1823bdd72ce6f0d219138f5124ef7645cedf8f59a1f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7edefb1d1e2fead8fd155f7b50a2cb49f2f3fed154ac3ef5f991ccaff93d6120",
+    "zh:807fb15b75910bf14795f2ad1a2d41b069f9ef52c242131b2964c8527312e235",
+    "zh:821d9148d261df1d1a8e5a4812df2a6a3ffaf0d2070dad3c785382e489069239",
+    "zh:a7d92251118fb723048c482154a6ac6368aad583d28d15fffc6f5dafd9507463",
+    "zh:b627d4cef192b3c12ddaf9cb2c4f98c10d0129883c8c2a9c0049983f9de7030d",
+    "zh:dfb70306fcc0ad1d512ab7c24765703783cc286062d4849de4fbe23526f5dc8e",
+    "zh:f21de276f857b7e51fa2593d8fef05a7faafb0a7b62db14ac58a03ce1be7d881",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.33.0"
+  constraints = "~> 5.33.0"
+  hashes = [
+    "h1:rAmKVvvzUqVocFppyheelWGnyfCcIGxLV31iFBY2sz4=",
+    "zh:10bb683f2a9306e881f51a971ad3b2bb654ac94b54945dd63769876a343b5b04",
+    "zh:3916406db958d5487ea0c2d2320012d1907c29e6d01bf693560fe05e38ee0601",
+    "zh:3cb54b76b2f9e30620f3281ab7fb20633b1e4584fc84cc4ecd5752546252e86f",
+    "zh:513bcfd6971482215c5d64725189f875cbcbd260c6d11f0da4d66321efd93a92",
+    "zh:545a34427ebe7a950056627e7c980c9ba16318bf086d300eb808ffc41c52b7a8",
+    "zh:5a44b90faf1c8e8269f389c04bfac25ad4766d26360e7f7ac371be12a442981c",
+    "zh:64e1ef83162f78538dccad8b035577738851395ba774d6919cb21eb465a21e3a",
+    "zh:7315c70cb6b7f975471ea6129474639a08c58c071afc95a36cfaa41a13ae7fb9",
+    "zh:9806faae58938d638b757f54414400be998dddb45edfd4a29c85e827111dc93d",
+    "zh:997fa2e2db242354d9f772fba7eb17bd6d18d28480291dd93f85a18ca0a67ac2",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:9f9e076b7e9752971f39eead6eda69df1c5e890c82ba2ca95f56974af7adfe79",
+    "zh:b1d6af047f96de7f97d38b685654f1aed4356d5060b0e696d87d0270f5d49f75",
+    "zh:bfb0654b6f34398aeffdf907b744af06733d168db610a2c5747263380f817ac7",
+    "zh:e25203ee8cedccf60bf450950d533d3c172509bda8af97dbc3bc817d2a503c57",
+  ]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -24,4 +24,107 @@ provider "aws" {
 
 locals {
   project = "hono_aws_hello_world"
+  dir_path = "${path.module}/../function/dist"
 }
+
+############################################################################
+## lambda
+############################################################################
+/* lambda実行ロール */
+# lambda用AWSマネージドポリシーを準備
+# ロギング用ポリシードキュメント
+data "aws_iam_policy_document" "logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["${aws_cloudwatch_log_group.lambda.arn}:*"]
+  }
+}
+
+# ロギング用ポリシー
+resource "aws_iam_policy" "logging" {
+  name        = "lambda-logging-policy"
+  description = "IAM policy for Lambda to write logs to CloudWatch"
+  policy      = data.aws_iam_policy_document.logging.json
+}
+
+# lambda assume用ポリシードキュメント
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# lambda用IAMロール作成
+resource "aws_iam_role" "lambda_execution_role" {
+  name               = "my-lambda-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+# lambda用IAMロールへポリシーをアタッチ
+resource "aws_iam_role_policy_attachment" "lambda_logs_attach" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.logging.arn
+}
+
+# ロググループを作成
+resource "aws_cloudwatch_log_group" "lambda" {
+  name              = "/aws/lambda/${local.project}_lambda"
+  retention_in_days = 14
+}
+
+# zipを作成
+data "archive_file" "lambda_my_function" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_dir       = local.dir_path
+  output_path      = "${local.dir_path}.zip"
+}
+
+/* lambda関数 */
+resource "aws_lambda_function" "lambda" {
+  function_name = "${local.project}_lambda"
+  role          = aws_iam_role.lambda_execution_role.arn
+
+  runtime  = "nodejs20.x" # TODO:Terraform古い
+  filename = data.archive_file.lambda_my_function.output_path
+  handler  = "handler.handler"
+
+  logging_config {
+    log_format = "Text"
+    log_group  = aws_cloudwatch_log_group.lambda.name
+  }
+
+  # Terraformに変更を無視させるため、lifecycle ルールを追加
+  lifecycle {
+    ignore_changes = [filename, source_code_hash]
+  }
+}
+
+# # 関数 URL
+# resource "aws_lambda_function_url" "public_url" {
+#   function_name      = aws_lambda_function.lambda.function_name
+#   authorization_type = "NONE"
+# }
+
+# # Function URL経由での呼び出しを許可 (authorization_type = "NONE" の場合必須)
+# resource "aws_lambda_permission" "allow_public_access" {
+#   statement_id  = "AllowPublicAccessToFunctionUrl"
+#   action        = "lambda:InvokeFunctionUrl"
+#   function_name = aws_lambda_function.lambda.function_name
+#   principal     = "*"
+#   function_url_auth_type = "NONE"
+
+#   # Function URLが作成された後に権限を付与
+#   depends_on = [aws_lambda_function_url.public_url]
+# }
